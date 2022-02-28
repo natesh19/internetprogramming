85109b1ec7e103bc1718d0acf9c986b95a4e84b7
Natesh Tamiri <natesh.kumar.1999@gmail.com>
2022-02-28 16:54:44 +0000

    newheadweek4

--- week4/alertbox.js
+++ week4/alertbox.js
@@ -1,11 +1,17 @@
 function print5(){
-    alert(1);
-    alert(2);
-    alert(3);
-    alert(4);
-    console.log
-    alert(5);
+   
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
 }
 print5();
 print5();
+function segment()
+{
+    var element = document.getElementById("newheading");
+    element.firstChild.nodeValue="New heading";
+}
+document.addEventListener('DOMContentLoaded',segment);
 
